4f54327d21a1dfd9d39c35e8632af5ddc1d3bd0e
Linh Quach <Linh.M.Quach1@gmail.com>
2021-06-25 13:35:27 -0500

    code updated

---
+++ prepare.py
@@ -0,0 +1,187 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# turn off pink warning boxes
+import warnings
+warnings.filterwarnings("ignore")
+
+#sklearn
+from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import explained_variance_score
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.model_selection import learning_curve
+
+
+
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[f'{col}'].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        return df[(df[f'{col}'] > lower_bound) & (df[f'{col}'] < upper_bound)]  
+    
+
+def handle_missing_values(df, prop_required_column = .6, prop_required_row = .5):
+    ''' 
+        take in a dataframe and a proportion for columns and rows
+        return dataframe with columns and rows not meeting proportions dropped
+    '''
+    col_thresh = int(round(prop_required_column*df.shape[0],0)) # calc column threshold
+    
+    df.dropna(axis=1, thresh=col_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    row_thresh = int(round(prop_required_row*df.shape[1],0))  # calc row threshhold
+    
+    df.dropna(axis=0, thresh=row_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    return df    
+    
+
+
+def miss_dup_values(df):
+    '''
+    this function takes a dataframe as input and will output metrics for missing values and duplicated rows,
+    and the percent of that column that has missing values and duplicated rows
+    '''
+        # Total missing values
+    mis_val = df.isnull().sum()
+        # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+        #total of duplicated
+    dup = df.duplicated().sum()
+        # Percentage of missing values
+    dup_percent = 100 * dup / len(df)
+        # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+    mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+        # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+    print( "  ")
+    print (f"** There are {dup} duplicate rows that represents {round(dup_percent, 2)}% of total Values**")
+        # Return the dataframe with missing information
+    return mis_val_table_ren_columns
+
+
+
+
+def split_zillow(df, target):
+    '''
+    this function takes in the zillow dataframe
+    splits into train, validate and test subsets
+    then splits for X (features) and y (target)
+    '''
+    # split df into 20% test, 80% train_validate
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=1234)
+    # split train_validate into 30% validate, 70% train
+    train, validate = train_test_split(train_validate, test_size=0.3, random_state=1234)
+    # Split with X and y
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+    return train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test
+
+
+
+
+# def get_heatmap(df, target):
+#     '''
+#     This method will return a heatmap of all variables and there relation to logerror
+#     '''
+#     plt.figure(figsize=(14,12))
+#     heatmap = sns.heatmap(df.corr()[[target]].sort_values(by=target, ascending=False), annot=True, linewidth=0.5,fmt = '.0%',cmap = 'mako', center = 0)
+#     heatmap.set_title('Feautures  Correlating with {}'.format(target))
+    
+#     return heatmap
+
+
+
+
+def get_heatmap(col_list, target,  color = 'mako'):
+    '''
+    This method will return a heatmap of all variables and there relation to logerror
+    '''
+    plt.figure(figsize=(14,12))
+    heatmap = sns.heatmap(col_list.corr()[[target]].sort_values(by=target, ascending=False), annot=True, linewidth=0.5,fmt = '.0%',cmap = color, center = 0)
+    heatmap.set_title('Feautures  Correlating with {}'.format(target))
+    
+    return heatmap
+
+
+
+def create_features(df):
+    df['age'] = 2017 - df.yearbuilt
+    df['age_bin'] = pd.cut(df.age, 
+                           bins = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140],
+                           labels = [0, .066, .133, .20, .266, .333, .40, .466, .533, 
+                                     .60, .666, .733, .8, .866, .933])
+
+    # create taxrate variable
+    df['taxrate'] = df.taxamount/df.taxvaluedollarcnt*100
+
+    # create acres variable
+    df['acres'] = df.lotsizesquarefeet/43560
+
+    # bin acres
+    df['acres_bin'] = pd.cut(df.acres, bins = [0, .10, .15, .25, .5, 1, 5, 10, 20, 50, 200], 
+                       labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9])
+
+    # square feet bin
+    df['sqft_bin'] = pd.cut(df.calculatedfinishedsquarefeet, 
+                            bins = [0, 800, 1000, 1250, 1500, 2000, 2500, 3000, 4000, 7000, 12000],
+                            labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                       )
+
+    # dollar per square foot-structure
+    df['structure_dollar_per_sqft'] = df.structuretaxvaluedollarcnt/df.calculatedfinishedsquarefeet
+
+
+    df['structure_dollar_sqft_bin'] = pd.cut(df.structure_dollar_per_sqft, 
+                                             bins = [0, 25, 50, 75, 100, 150, 200, 300, 500, 1000, 1500],
+                                             labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                            )
+
+
+    # dollar per square foot-land
+    df['land_dollar_per_sqft'] = df.landtaxvaluedollarcnt/df.lotsizesquarefeet
+
+    df['lot_dollar_sqft_bin'] = pd.cut(df.land_dollar_per_sqft, bins = [0, 1, 5, 20, 50, 100, 250, 500, 1000, 1500, 2000],
+                                       labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                      )
+
+
+    # update datatypes of binned values to be float
+    df = df.astype({'sqft_bin': 'float64', 'acres_bin': 'float64', 'age_bin': 'float64',
+                    'structure_dollar_sqft_bin': 'float64', 'lot_dollar_sqft_bin': 'float64'})
+
+
+    # ratio of bathrooms to bedrooms
+    df['bath_bed_ratio'] = df.bathroomcnt/df.bedroomcnt
+
+    return df
